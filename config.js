1894537ce919f15a0d40d220b1086b736e35b4f5
hoc-anms <phamvanhoc96@gmail.com>
2018-11-27 18:03:54 +0700

    api abc

--- config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 8888,
-    URL: process.env.BASE_URL || 'http://localhost:8888',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://YOUR_URI',
-    MONGODB_ATLAS_PW: process.env.MONGODB_ATLAS_PW || 'admin'
-};
